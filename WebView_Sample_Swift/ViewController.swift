b8956477f779f7863899aea6b80ec7b6b276ce71
Sarah Jeong <pishon313@gmail.com>
2024-07-05 00:39:03 +0900

    Initial Commit

---
+++ WebView_Sample_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebView_Sample_Swift
+//
+//  Created by Sarah Jeong on 7/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WebView_Sample_SwiftUITests/WebView_Sample_SwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WebView_Sample_SwiftUITestsLaunchTests.swift
+//  WebView_Sample_SwiftUITests
+//
+//  Created by Sarah Jeong on 7/5/24.
+//
+
+import XCTest
+
+final class WebView_Sample_SwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
